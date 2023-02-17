2e0436e39a68169098575c1f11dee95a57fd9322
Mahesh90918 <mahesh996345@gmail.com>
2023-02-17 11:47:46 +0530

    Mail Api

--- 19SpringBoot_MailApi/src/main/java/com/mahesh/Controller/MailController.java
+++ 19SpringBoot_MailApi/src/main/java/com/mahesh/Controller/MailController.java
@@ -5,6 +5,7 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,8 +26,5 @@ public class MailController {
 	public  @ResponseBody String Hiii() {
 		return "HIIII";
 	}
-//	@GetMapping("/sentMail1/{sentMail}")
-//	public  String SendMailMsg1(@PathParam("sentMail") String sentMail) {
-//		return mailSendUtil.senderMail(sentMail);
-//	}
+	
 }
--- 19SpringBoot_MailApi/src/main/java/com/mahesh/Controller/MailController2.java
+++ 19SpringBoot_MailApi/src/main/java/com/mahesh/Controller/MailController2.java
@@ -1,7 +1,10 @@
 package com.mahesh.Controller;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,20 +14,30 @@ import com.mahesh.MailUtil.MailSendUtil;
 
 @RestController
 public class MailController2 {
-	
+
 	@Autowired
 	MailSendUtil mailSendUtil;
 
 	@GetMapping("/sentMail1")
-	public  String SendMailMsg(@RequestParam("gmail") String sentMail) {
+	public String SendMailMsg(@RequestParam("gmail") String sentMail) {
 		return mailSendUtil.senderMail(sentMail);
 	}
+
 	@GetMapping("/hii1")
-	public  String Hiii() {
+	public String Hiii() {
 		return "HIIII";
 	}
-//	@GetMapping("/sentMail1/{sentMail}")
-//	public  String SendMailMsg1(@PathParam("sentMail") String sentMail) {
-//		return mailSendUtil.senderMail(sentMail);
-//	}
+
+	@GetMapping("/sent2/{sentMail}")
+	public String SendMailMsg2(@PathVariable String sentMail) {
+		System.out.println("Sent2" + sentMail);
+		return mailSendUtil.senderMail(sentMail);
+	}
+
+	@GetMapping("/sent3")
+	public String SendMailMsg1(@PathParam("gmail") String gmail) {
+		System.out.println("Sent1" + gmail);
+		return mailSendUtil.senderMail(gmail);
+	}
+
 }
