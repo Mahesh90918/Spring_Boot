5d3a5005cf4e1ea3b5bca3ca62be2a160387545d
Mahesh90918 <mahesh996345@gmail.com>
2023-02-18 18:42:47 +0530

    Spring Boot Admin

---
+++ 22Springboot-Admin-client/src/main/java/com/mahesh/service/UserInfoImpl.java
@@ -0,0 +1,52 @@
+package com.mahesh.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mahesh.dao.UserRepo;
+import com.mahesh.model.UserInfo;
+@Service
+public class UserInfoImpl implements UserService{
+
+	@Autowired
+	UserRepo repo;
+	
+	@Override
+	public void addUser(UserInfo info) {
+		repo.save(info);
+		
+	}
+
+	@Override
+	public List<UserInfo> getAllUsers() {
+	
+		return repo.findAll();
+	}
+
+	@Override
+	public UserInfo getById(Integer id) {
+	
+		return repo.findById(id).get();
+	}
+
+	@Override
+	public void deleteUser(Integer id) {
+	repo.deleteById(id);
+		
+	}
+
+	@Override
+	public void updateUser(UserInfo user) {
+	repo.save(user);
+		
+	}
+
+	@Override
+	public UserInfo findByUserName(String name) {
+		// TODO Auto-generated method stub
+		return repo.findByUserName(name);
+	}
+
+}
